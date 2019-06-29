4314c89d4fcc4bc487c9ea6c11e91aeee11326e6
Peach Tuntiwong <peachtuntiwong@Peachs-MacBook-Pro.local>
2019-06-30 01:12:06 +0700

    initialise redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 
 import App from './App';
+import store from './config/store';
 
-const app = <BrowserRouter>
-	<App />
-</BrowserRouter>
+const app = <Provider store={store}>
+	<BrowserRouter>	
+		<App />
+	</BrowserRouter>	
+</Provider>
 
 ReactDOM.render(app, document.getElementById('root'));
