92c511d8f5df1784449ee7ac63b7b1360dcef602
Peach Tuntiwong <peachtuntiwong@Peachs-MacBook-Pro.local>
2019-06-30 01:00:41 +0700

    navigation bar added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom'
 
 import Router from './Router.js'
+
+const Navigation = (props) => <nav>
+	<ul>
+		<li><NavLink to='/'>Home</NavLink></li>
+		<li><NavLink to='/checkout'>Checkout</NavLink></li>
+	</ul>
+</nav>
 
 class App extends Component {
 	render() {
 		return <div>
-      	<h1>Shopping Cart</h1>
-      	<Router />
-    </div>
+	      	<h1>Shopping Cart</h1>
+	      	<Navigation />
+	      	<Router />
+	    </div>
 	}
 }
 
